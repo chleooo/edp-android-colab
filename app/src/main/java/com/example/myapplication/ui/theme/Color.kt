5a6df7b188538617396398161c225c9b2d29a02c
Chloe Monteza <cnmonteza64178@liceo.edu.ph>
2026-07-29 10:33:00 +0800

    Prelim hands-on: my profile screen

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Maroon Palette
+val BrandMaroon = Color(0xFF800020)
+val LightMaroonAccent = Color(0xFFF8D7DA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme
+val LightPrimary = BrandMaroon
+val LightOnPrimary = Color.White
+val LightPrimaryContainer = LightMaroonAccent
+val LightSecondary = Color(0xFFA63D5A)
+val LightSurface = Color.White
+val LightOnSurface = Color(0xFF1E1E1E)
+val LightOnSurfaceVariant = Color(0xFF5F5F5F)
+
+// Dark Theme
+val DarkPrimary = Color(0xFF5C0013)
+val DarkOnPrimary = Color.White
+val DarkPrimaryContainer = Color(0xFF3B000C)
+val DarkSecondary = Color(0xFFD48A9A)
+val DarkSurface = Color(0xFF121212)
+val DarkOnSurface = Color.White
+val DarkOnSurfaceVariant = Color(0xFFBDBDBD)
